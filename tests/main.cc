3c70ec288ad24d15a7ef9957cd5fb751e6bbb239
Justin Berger <jberger@melco.com>
2016-12-05 16:16:50 -0700

    Added more test cases; otherwise this thing is somewhat obnoxious

--- tests/main.cc
+++ tests/main.cc
@@ -1,12 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include "SpellCheck.h"
-
+#include <chrono>
 #include "Catch/Catch.h"
 
 // TODO: These unit tests are a good starting point, feel free to add others as you go but these are expected to pass
 TEST_CASE( "Distance Test", "[spellcheck]" ) {
+	REQUIRE( SpellCheck::Distance("abcde", "zzzabcde") == 3);
+	REQUIRE( SpellCheck::Distance("abcdefg", "zzzabcdefg") == 3);
+	REQUIRE( SpellCheck::Distance("george", "forget") == 3);
+	REQUIRE( SpellCheck::Distance("abcdefg", "abczzzz") == 4);
+	REQUIRE( SpellCheck::Distance("zzzabcdefg", "abcdefg") == 3);
+	REQUIRE( SpellCheck::Distance("profesior", "profession") == 2);
 	REQUIRE( SpellCheck::Distance("Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet") == 0 );
-	REQUIRE( SpellCheck::Distance("dog", "dgo") == 1 );
+	REQUIRE( SpellCheck::Distance("dog", "dgo") == 2 );
 	REQUIRE( SpellCheck::Distance("Dog", "dog") == 1 );
 }
 
@@ -19,5 +25,20 @@ TEST_CASE( "IsKnownWord Test", "[spellcheck]" ) {
 }
 
 TEST_CASE( "Replacements Test", "[spellcheck]" ) {
-	REQUIRE(  SpellCheck::Replacements("dgo") == std::vector<std::string>({"dog"}) );
+	REQUIRE(  SpellCheck::Replacements("dogg", 1) == std::vector<std::string>({"dog"}) );
+	REQUIRE(  SpellCheck::Replacements("profesior", 3) == std::vector<std::string>({ "professor", "profession" }) );
+	REQUIRE(  SpellCheck::Replacements("george", 2) == std::vector<std::string>({ "George" }));
+}
+
+
+TEST_CASE( "Speed check", "[spellcheck]") {
+	using namespace std::chrono;
+
+	auto now = std::chrono::high_resolution_clock::now();
+	for(auto i = 0;i < 5000;i++) {
+		REQUIRE(  SpellCheck::Replacements("zzzzzzzzzzzz", 5) == std::vector<std::string>({}) );
+	}
+	duration<double> timespan = duration_cast<duration<double>>(std::chrono::high_resolution_clock::now() - now);
+
+	std::cerr << "Elapsed seconds " << timespan.count() << std::endl;
 }
